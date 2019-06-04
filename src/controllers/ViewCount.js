aa81b3bb13351b6bc1073f6af81f2fb9fe83fe19
Ilya Lebedev <bacherko@gmail.com>
2019-06-04 18:58:16 +0300

    feat: use core metrics #14

--- src/controllers/ViewCount.js
+++ src/controllers/ViewCount.js
@@ -1,47 +1,16 @@
-const express = require('express');
-const client = require('prom-client');
-
 const core = require('gls-core-service');
 const { Post, View } = require('../model');
 
 const BasicController = core.controllers.Basic;
 
-const server = express();
-
-client.collectDefaultMetrics({ timeout: 5000 });
-
-const callCounter = new client.Counter({
-    name: 'api_getpostsviewcount_total',
-    help: 'api call count',
-});
-
-const callTime = new client.Gauge({
-    name: 'api_getpostsviewcount_time',
-    help: 'api processing time',
-});
-callTime.setToCurrentTime();
-
-const hist2 = new client.Histogram({
-    name: 'api_getpostsviewcount_time_hist',
-    help: 'api processing time histogram',
-    buckets: [0.1, 5, 15, 50, 100, 500],
-});
-
 class ViewCount extends BasicController {
     async getPostsViewCount({ postLinks }) {
-        callCounter.inc();
-
-        console.log('inced');
-
         if (!postLinks) {
             throw {
                 code: 1110,
                 message: 'Invalid params',
             };
         }
-
-        const end = callTime.startTimer();
-        const endHist = hist2.startTimer();
 
         const results = [];
 
@@ -53,9 +22,6 @@ class ViewCount extends BasicController {
                 });
             })
         );
-
-        end();
-        endHist();
 
         return {
             results,
@@ -110,16 +76,3 @@ class ViewCount extends BasicController {
 }
 
 module.exports = ViewCount;
-
-server.get('/metrics', (req, res) => {
-    res.set('Content-Type', client.register.contentType);
-    res.end(client.register.metrics());
-});
-
-server.get('/metrics/counter', (req, res) => {
-    res.set('Content-Type', client.register.contentType);
-    res.end(client.register.getSingleMetricAsString('api_getpostsviewcount_count_2'));
-});
-
-console.log('Server listening to 3000, metrics exposed on /metrics endpoint');
-server.listen(9777);
